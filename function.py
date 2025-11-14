a40f9916bc862158a613ec55a00cff6a0b7403db
ya_xd_lol <vitalijbilyj4@gmail.com>
2025-11-14 21:10:11 +0100

    finished function class

--- function.py
+++ function.py
@@ -5,14 +5,20 @@ class Functions:
 
 	def __init__(self):
 		self.functions = {}
-		self.get_def()
+		self.__get_def()
 
-	def get_def(self):
+	def __get_def(self):
 		try:
 			with open ("data/functions.json", "r") as o:
 				self.functions = json.load(o)
 		except:
 			raise "You maybe have delete this file, try to reinstall system"
-				
 
-f = Functions()
+	def exec_func(self, name, *args):
+		with open("data/functions.json") as r:
+			code = json.load(r)
+			code = code[name]
+			print(code)
+		env = {}
+		exec(code, env)
+		env[name](*args)
